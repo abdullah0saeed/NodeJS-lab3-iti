7913c212df7cbfb6c11bd52358f4e4665e83e90c
abdullah0saeed <86889145+abdullah0saeed@users.noreply.github.com>
2025-10-11 12:25:30 +0300

    fix: correct restrictTo argument format in deletePost route

--- routes/post.js
+++ routes/post.js
@@ -17,7 +17,7 @@ const restrictTo = require("../middlewares/restrictTo");
 
 router.get("/:id", getPostById);
 router.put("/:id", validateUpdatePost, updatePost);
-router.delete("/:id", restrictTo("admin"), deletePost);
+router.delete("/:id", restrictTo(["admin"]), deletePost);
 router.post("/", validateCreatePost, createPost);
 router.get("/", getPosts);
 
